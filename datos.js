f0bc3f24778924569133a74a07bb4867dd374bf2
lapausacafeytapas <lapausacafeytapas@gmail.com>
2026-07-05 16:02:46 +0200

    Añadir productos a la categoría La PAUsa de las noches

--- datos.js
+++ datos.js
@@ -313,7 +313,134 @@ const PRODUCTOS = {
     }
   ],
 
-  viernesysabado: [],
+  viernesysabado: [
+    {
+      nombre: "Mix de lechugas con queso de cabra, bacon, nueces, pipas y manzana",
+      descripcion: "",
+      precio: "9,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Melón, crujiente de jamón, lechuga, aceitunas negras y semillas",
+      descripcion: "",
+      precio: "9,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Bravas",
+      descripcion: "",
+      precio: "6,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Revuelto de trilogía de setas.",
+      descripcion: "",
+      precio: "5,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Alcachofas confitadas con crujiente de jamón (2 ud)",
+      descripcion: "",
+      precio: "6,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Torrezno",
+      descripcion: "",
+      precio: "7,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Croquetas",
+      descripcion: "Jamón. Manzana y queso gorgonzola. Guarra y miel.",
+      precio: "2,20 / ud.",
+      alergenos: "",
+      por_unidad: true
+    },
+    {
+      nombre: "Calamar a la andaluza",
+      descripcion: "",
+      precio: "12,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Sepia a la plancha con salsa mery",
+      descripcion: "",
+      precio: "12,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Huevos rotos con sobrasada y miel",
+      descripcion: "",
+      precio: "7,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Huevos rotos con crujiente de jamón",
+      descripcion: "",
+      precio: "7,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Choricitos a la sidra",
+      descripcion: "",
+      precio: "7,75",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Dados de solomillo de cerdo con reducción de Pedro Ximénez",
+      descripcion: "",
+      precio: "9,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Tellinas",
+      descripcion: "",
+      precio: "13,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Fritura de boquerones",
+      descripcion: "",
+      precio: "6,50",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Pechuga rebozada con guarnición de patatas fritas",
+      descripcion: "",
+      precio: "7,00",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Trío de creps",
+      descripcion: "Crep de espinacas relleno de queso de cabra, nueces y miel. Crep de zanahoria relleno de jamón y queso emmental. Crep de remolacha relleno de pollo al curry.",
+      precio: "12,75",
+      alergenos: "",
+      por_unidad: false
+    },
+    {
+      nombre: "Pan en aceite de oliva virgen extra",
+      descripcion: "",
+      precio: "0,75 / pax.",
+      alergenos: "",
+      por_unidad: true
+    }
+  ],
 
   bebidas: {
     refrescos: [
